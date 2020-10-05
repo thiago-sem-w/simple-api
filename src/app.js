195a794bef4680ed6a6d1206926749d1d2aa8edc
Thiago Sem W <thiagosemw@gmail.com>
2020-10-05 17:33:21 -0300

    add error handler

--- src/app.js
+++ src/app.js
@@ -8,4 +8,16 @@ const port = process.env.PORT || 5000
 
 app.set('port', port)
 
+app.use('*', async (req, res, next) => {
+	res.status(404)
+	next(new Error('endpoint not exist'))
+})
+
+app.use(async (err, req, res, next) => {
+	const statusCode = res.statusCode || 500
+	res
+		.status(statusCode)
+		.json({ error: err.message })
+})
+
 module.exports = app
